2a60ab3b5b6cd9e6ed96c8115bc4144a59943861
TechShiva <shaileng86@gmail.com>
2016-11-11 18:37:21 +0530

    my javaScript practice

---
+++ myScript.js
@@ -0,0 +1,52 @@
+
+//document.querySelector("p").style.cssText.fontcolor("red");
+//document.getElementById("para").style.cssText.bold();
+ //prompt("Are you ready....??","");
+  
+/*  var myNumber = Number(prompt("Input any number...."));
+  alert("You have entered " + myNumber);                              */
+
+ //FizzBuzz program......
+ /*
+var i;
+for(i=1;i<=100;i++)
+{
+    if( (i%3 == 0) && (i%5==0) )
+    {
+        document.writeln("BuzzFizz"+" "+i);
+    }
+    else if((i%3) == 0)
+    {
+        document.writeln("Fizz"+" "+i);
+    }
+    else if((i%5)==0)
+    {
+        document.writeln("Buzz"+" "+i);
+    }
+}                                                    */                     
+
+/* Chess board
+
+var hight;
+var width;
+var chess=" ";
+
+for(hight=1;hight<=8;hight++)
+{
+    for(widht=1;width<8;width++)
+    {
+        chess = chess + "*";
+        document.writeln(chess);
+    }
+}                                             */        
+                                            
+//document.write("Good");
+
+/*
+function greet(who)
+{
+    console.log("Hello "+who);
+}
+greet("Harry");
+console.log("Bye");                          */
+
