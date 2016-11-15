fecf9a07b2fa181227740cde8816cc398f9ecc01
TechShiva <shaileng86@gmail.com>
2016-11-15 17:40:27 +0530

    Display using head tag

--- myScript.js
+++ myScript.js
@@ -7,7 +7,7 @@ function add()
     var sn = Number(document.getElementById("num2").value);
     var result = fn + sn;
     //window.alert(result);
-    var display = document.createElement("h2");
+    var display = document.getElementById("dis");
     display.textContent = result;
 }
 function sub()
@@ -15,19 +15,25 @@ function sub()
     var fn = Number(document.getElementById("num1").value);
     var sn = Number(document.getElementById("num2").value);
     var result = fn - sn;
-    window.alert(result);
+    //window.alert(result);
+    var display = document.getElementById("dis");
+    display.textContent = result;
 }
 function mul()
 {
     var fn = Number(document.getElementById("num1").value);
     var sn = Number(document.getElementById("num2").value);
     var result = fn * sn;
-    window.alert(result);
+    //window.alert(result);
+    var display = document.getElementById("dis");
+    display.textContent = result;
 }
 function div()
 {
     var fn = Number(document.getElementById("num1").value);
     var sn = Number(document.getElementById("num2").value);
     var result = fn / sn;
-    window.alert(result);
+    //window.alert(result);
+    var display = document.getElementById("dis");
+    display.textContent = result;
 }                                                                 
